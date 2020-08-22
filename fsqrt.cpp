6057d054e94a494d56d632035281116743a00f4e
bsdoshi01 <bsdoshi01@gmail.com>
2020-08-22 10:45:22 +0530

    Finding Square Roots

---
+++ fsqrt.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fsqrt() {
+	int n;
+	cin >> n;
+	cout << (int)sqrt(n) << endl;
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) 
+		fsqrt();
+	return 0;
+}
